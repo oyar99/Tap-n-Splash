170eb116bcce8132007dab4a61b42f0b955c5542
Jhon Rayo <RayoJhon99@gmail.com>
2019-05-01 16:12:49 +1000

    Add settings controller

---
+++ Bubble Pop/Bubble Pop/Scenes/Settings.h
@@ -0,0 +1,17 @@
+//
+//  Settings.h
+//  Bubble Pop
+//
+//  Created by Jhon Stewar Rayo Mosquera on 1/5/19.
+//  Copyright © 2019 Jhon Stewar Rayo Mosquera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Settings : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
